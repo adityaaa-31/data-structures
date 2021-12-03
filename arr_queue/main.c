17703e68d9db60fb0db58f34e836d4d38807daac
adityaaa-31 <adityavshedge3101@gmail.com>
2021-12-03 14:27:43 +0530

    Queue using arrays

---
+++ arr_queue/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 5
+
+struct queue{
+
+    int data[MAX], front rear;
+};
+
+void initq(struct queue *q)
+{
+    q -> front = q -> rear = -1;
+}
+
+int isEmpty(struct queue *q)
+
+int main()
+{
+
+    return 0;
+}
